9f0fbc8486d6b7a60f27688f1322d3bd99561cbd
Kirill Sukhorukov <kirill.suhorukov@gmail.com>
2017-11-19 20:39:26 +0300

    schedule: sort slots

--- lib/schedule.rb
+++ lib/schedule.rb
@@ -1,6 +1,23 @@
-require_relative "./slot"
+require 'byebug'
 
 class Schedule
-  def initialize
+  attr_reader :slots, :first_room, :second_room
+
+  def initialize(slots)
+    @slots = slots
+    @first_room = []
+    @second_room = []
+    build
+  end
+
+  def build
+    sort_slots
+  end
+
+  def sort_slots
+    @slots = slots.sort_by { |slot| slot.duration * -1 }
+  end
+
+  def print
   end
 end
--- rooms.rb
+++ rooms.rb
@@ -5,10 +5,11 @@ require 'byebug'
 require './lib/input_processor.rb'
 require './lib/schedule'
 require './lib/usage'
-require './lib/slot'
 
 usage if ARGV.count != 1 
 
 input = InputProcessor.new(ARGV[0])
+schedule = Schedule.new(input.slots)
+
 
 
